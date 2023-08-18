42727b3c23864d88ab5335159f81dece5a48a95d
Mohamed-hussain-pro <mohamedesamworld1@gmail.com>
2023-08-18 13:24:11 +0200

    reorganize the imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,21 @@
+// Modules
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
+// Angular material modules
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatIconModule } from '@angular/material/icon';
-import { MeasurementComponent } from './components/measurement/measurement.component';
-import { RouterModule } from '@angular/router';
 import {MatTableModule} from '@angular/material/table';
+// Components
+import { AppComponent } from './app.component';
+import { MeasurementComponent } from './components/measurement/measurement.component';
 import { BarChartComponent } from './components/bar-chart/bar-chart.component';
 import { LineChartComponent } from './components/line-chart/line-chart.component';
-import { CollapseModule } from 'ngx-bootstrap/collapse';
 import { NavbarComponent } from './components/navbar/navbar.component'; // Import CollapseModule
 
 @NgModule({
